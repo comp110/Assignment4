a7b3dfa0f019fc28f97c7d156e622943dc7df6d3
Kris Jordan <krisjordan@gmail.com>
2015-11-10 01:30:05 -0500

    Updates to include Emily's changes.

--- src/com/comp110/instagram/Color.java
+++ src/com/comp110/instagram/Color.java
@@ -1,7 +1,25 @@
 package com.comp110.instagram;
 
 public class Color {
-
   
+  // TODO: Define private instance variables
+  
+  public Color() {
+    this.setRed(1.0);
+    this.setGreen(1.0);
+    this.setBlue(1.0);
+  }
+  
+  public Color(double red, double green, double blue) {
+    this.setRed(red);
+    this.setGreen(green);
+    this.setBlue(blue);
+  }
+  
+  public String toString() {
+    return "Color("+this.getRed()+","+this.getGreen()+","+this.getBlue()+")";
+  }
+  
+  // TODO: Add other instance definitions
   
 }
--- src/com/comp110/instagram/Image.java
+++ src/com/comp110/instagram/Image.java
@@ -1,7 +1,17 @@
 package com.comp110.instagram;
 
 public class Image {
-
   
+  private Color[][] pixels;
+
+  public Image(int width, int height) {
+    // TODO: Implement this constructor
+  }
+  
+  public String toString() {
+    return "Image("+this.getWidth()+","+this.getHeight()+")";
+  }
+  
+  // TODO: Add other instance definitions
   
 }
